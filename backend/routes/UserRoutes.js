3cf05c1016b57492c380fb7dee944c087a8f59e1
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-01-05 22:14:45 -0300

    initialized validation

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,7 +4,11 @@ const router = express.Router();
 // Controllers
 const { register } = require("../controllers/UserController");
 
+// Middlewares
+const validate = require("../middlewares/handleValidation");
+const { userCreatedValidation } = require("../middlewares/userValidations");
+
 // Routes
-router.post("/register", register);
+router.post("/register", userCreatedValidation(), validate, register);
 
 module.exports = router;
