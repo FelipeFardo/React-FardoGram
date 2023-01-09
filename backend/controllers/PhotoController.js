2c97e6b8a7850e81da35cde3bed572f7b7e96c21
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-01-08 23:13:55 -0300

    search photos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -179,6 +179,12 @@ const commentPhoto = async (req, res) => {
   }
 };
 
+// Search photos by title
+const searchPhotos = async (req, res) => {
+  const { q } = req.query;
+  const photos = await Photo.find({ title: new RegExp(q, "i") }).exec();
+  res.status(200).json(photos);
+};
 module.exports = {
   insertPhoto,
   deletePhoto,
@@ -188,4 +194,5 @@ module.exports = {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos,
 };
