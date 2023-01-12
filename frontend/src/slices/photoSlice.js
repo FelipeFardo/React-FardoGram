e7784ac2dea70c2faa23b319e1bd6a74fd406a8c
Felipe Fardo <felipe.fardo@hotmail.com.br>
2023-01-12 17:13:01 -0300

    home

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -117,6 +117,13 @@ export const comment = createAsyncThunk(
   }
 );
 
+// Get all photos
+export const getPhotos = createAsyncThunk("photo/getall", async (thunkAPI) => {
+  const token = thunkAPI.getState().auth.user.token;
+  const data = await photoService.getPhotos(token);
+  return data;
+});
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -237,6 +244,16 @@ export const photoSlice = createSlice({
       .addCase(comment.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
+      })
+      .addCase(getPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getPhotos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = action.payload;
       });
   },
 });
